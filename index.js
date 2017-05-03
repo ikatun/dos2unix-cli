72939e2a202682ead2747d1007c427999e23b06a
Ivo Katunarić <ivo@shoutem.com>
2017-05-03 14:52:16 +0200

    Use 'fs' instead of linefeeds module

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
 const yargs = require('yargs');
-const lf = require('linefeeds');
 const globArray = require('glob-array');
 const path = require('path');
+const fs = require('fs');
 
 const args = yargs
   .usage('Usage: dos2unix <paths...>')
@@ -12,10 +12,10 @@ const args = yargs
 
 const files = globArray.sync(args._);
 
-console.log('Converting:')
+console.log('Converting:');
 files.map(file => {
   console.log('  ' + file);
-  lf.convertSync(file, { ending: lf.lf });
+  const content = fs.readFileSync(file, 'utf8');
+  fs.writeFileSync(file, Buffer.from(content.replace(/\r\n/g, '\n'), 'utf8'));
 });
 console.log('done');
-
